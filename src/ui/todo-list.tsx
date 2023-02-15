7fcb9e2b5c7724c7518369186bd62bdda5c073ca
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-02-15 18:40:34 +0700

    add theming

--- src/ui/todo-list.tsx
+++ src/ui/todo-list.tsx
@@ -3,11 +3,6 @@ import { useRef } from 'react'
 import { Todo, TodosStore } from '../todo-store'
 import styles from './todo-list.module.css'
 
-type TodoListProps = {
-  todoList: TodosStore
-}
-
-
 const TodoForm = ({onCreate}: {onCreate: (title: string) => void}) => {
   const ref = useRef<HTMLInputElement>(null)
   const create = () => {
@@ -18,28 +13,29 @@ const TodoForm = ({onCreate}: {onCreate: (title: string) => void}) => {
   }
   return (
     <div>
-      <input type="text" className={styles['todo-input']} ref={ref}/>
+      <input type="text" className={styles.todoInput} ref={ref}/>
       <button onClick={create}>Add task</button>
     </div>
     )
 }
 
-
-const TodoList = observer(({todoList}: TodoListProps) => (
+const TodoList = observer(({todoList}: {
+  todoList: TodosStore
+}) => (
   <div>
     <TodoForm onCreate={(title: string) => todoList.add(title)}/>
-    <ul className={styles['list']}>
+    <ul className={styles.list}>
       {todoList.todos.map((todo: Todo) => (
-        <li className={styles['list-item']} key={todo.id}>
+        <li className={styles.listItem} key={todo.id}>
           <input
             className={styles.todoToggle}
             type="checkbox"
             checked={todo.done}
             onChange={() => todoList.toggle(todo.id)}
           />
-          <span className={styles['todoTitle']} onDoubleClick={() => todoList.edit(todo.id, prompt('Change title', todo.title))}>{todo.title}</span>
+          <span className={styles.todoTitle} onDoubleClick={() => todoList.edit(todo.id, prompt('Change title', todo.title))}>{todo.title}</span>
           <div style={{flexGrow: 1}}/>
-          <button  className={styles['todoDelete']} onClick={() => todoList.remove(todo.id)}>✖️</button>
+          <button  className={styles.todoDelete} onClick={() => todoList.remove(todo.id)}>✕</button>
         </li>
       ))}
     </ul>
